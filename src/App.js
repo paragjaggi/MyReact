5a4a385ae93e55e15038015ddb6e59f803b8813c
paragjaggi <parag.jaggi@hcl.com>
2021-03-31 15:07:31 +0530

    Updated for state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import './App.css';
+import React from 'react';
 import MyAnime from './MyAnime/MyAnime';
 import Header from './MyAnime/header';
 import SideBar from './MyAnime/sidebar';
 import Footer from './MyAnime/footer';
 
-const state = {
-  name : 'Parag Jaggi',
-  siteName : 'MyAnime'
-}
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'Parag Jaggi',
+      siteName: 'MyAnime'
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <Header siteName = {state.siteName}/>
-      <MyAnime />
-      <Footer name={state.name}/>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <Header siteName={this.state.siteName} />
+        <MyAnime />
+        <Footer name={this.state.name} />
+      </div>
+    );
+
+  }
 }
 
 export default App;
